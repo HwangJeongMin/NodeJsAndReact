6e94fb722ffd7f141eb5a675bc2a3edffae28f8d
HwangJeongMin <hjmkona@naver.com>
2020-12-23 08:43:47 +0900

    nodeJS 공부중 비밀번호 암호화 잘 안됨

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 5;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,6 +31,29 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+userSchema.pre("save", (next) => {
+  let user = userSchema.tree;
+  // if (user.isModified(password)) {
+  // 비밀번호를 암호화 시킨다.
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    if (err) return next(err);
+
+    // console.log(user.password, salt);
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+
+  // bcrypt.genSalt(saltRounds, function (err, salt) {
+  //   bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
+  //     // Store hash in your password DB.
+  //   });
+  // });
+  // }
 });
 
 const User = mongoose.model("User", userSchema);
