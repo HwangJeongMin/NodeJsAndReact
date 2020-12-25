36763c8ec8e2e14f10387977bd4e42b86e704b02
HwangJeongMin <hjmkona@naver.com>
2020-12-25 11:02:53 +0900

    기본강의 완료

--- server/models/User.js
+++ server/models/User.js
@@ -35,13 +35,14 @@ const userSchema = mongoose.Schema({
 });
 
 userSchema.pre("save", function (next) {
-  let user = this;
+  var user = this;
   if (user.isModified("password")) {
+    //비밀번호를 암호화 시킨다.
     bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
+
       bcrypt.hash(user.password, salt, function (err, hash) {
         if (err) return next(err);
-        // Store hash in your password DB.
         user.password = hash;
         next();
       });
@@ -52,7 +53,7 @@ userSchema.pre("save", function (next) {
 });
 
 userSchema.methods.comparePassword = function (plainPassword, cb) {
-  // plainPassword
+  //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
   bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
     if (err) return cb(err);
     cb(null, isMatch);
@@ -60,9 +61,15 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
 };
 
 userSchema.methods.generateToken = function (cb) {
-  let user = this;
-  // jsonwebtoken을 이용해서 토큰을 생성
-  let token = jwt.sign(user._id.toHexString(), "secretToken");
+  var user = this;
+  // console.log('user._id', user._id)
+
+  // jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  // user._id + 'secretToken' = token
+  // ->
+  // 'secretToken' -> user._id
+
   user.token = token;
   user.save(function (err, user) {
     if (err) return cb(err);
@@ -70,14 +77,13 @@ userSchema.methods.generateToken = function (cb) {
   });
 };
 
-userSchema.methods.findByToken = function (token, cb) {
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
   // 토큰을 decode 한다.
   jwt.verify(token, "secretToken", function (err, decoded) {
-    if (err) return cb(err);
-
     // 유저 아이디를 이용해서 유저를 찾은 다음
     // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
-    User.findOne({ _id: decoded, token: token }, function (err, user) {
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
       if (err) return cb(err);
       cb(null, user);
     });
